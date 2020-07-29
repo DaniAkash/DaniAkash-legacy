44386a65bd7e1cdd4463fad720c53d4eb6cf0801
DaniAkash <s.daniakash@gmail.com>
2020-07-29 16:30:59 +0530

    updated box dimensions

--- api/get-trello-cards.js
+++ api/get-trello-cards.js
@@ -12,10 +12,29 @@ export default (req, res) => {
       res.setHeader("Content-Type", "image/svg+xml");
       res.status(200).send(
         `
-        <svg fill="none" viewBox="0 0 800 400" width="800" height="400" xmlns="http://www.w3.org/2000/svg">
+        <svg width="350" height="170" viewBox="0 0 350 170" fill="none" xmlns="http://www.w3.org/2000/svg">
           <foreignObject width="100%" height="100%">
             <div xmlns="http://www.w3.org/1999/xhtml">
               <style>
+              ul {
+                box-sizing: border-box;
+                color: rgb(36, 41, 46);
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji;
+                font-size: 16px;
+                line-height: 24px;
+                margin-bottom: 16px;
+                margin-top: 0px;
+                overflow-wrap: break-word;
+                padding-left: 32px;
+              }
+              li {
+                box-sizing: border-box;
+                color: rgb(36, 41, 46);
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji;
+                font-size: 16px;
+                line-height: 24px;
+                overflow-wrap: break-word;
+              }
               </style>
               <ul>
                 ${list.reduce((acc, each) => acc + each, "")}
